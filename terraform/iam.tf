bc443ffaf05931dc4473c708e4883db348f7a29d
farrisismatiteach <farris.ismati.teach@gmail.com>
2023-03-08 10:46:25 -0500

    added iam

---
+++ terraform/iam.tf
@@ -0,0 +1,53 @@
+# role task execution role
+data "aws_iam_policy_document" "assume_role_policy" {
+    statement {
+      actions = ["sts:AssumeRole"]
+      effect="allow"
+      principals {
+        type = "Service"
+        identifiers = ["lambda.amazonaws.com"]
+      }
+    }
+}
+
+resource "aws_iam_role" "lambda_main" {
+    name = "${var.app_name}-lambda"
+    assume_role_policy = data.aws_iam_policy_document.assume_role_policy
+}
+
+resource "aws_iam_role_policy_attachment" "attach_exec_role" {
+    role = aws_iam_role.lambda_main.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "aws_iam_policy_document" "lambda_ws" {
+    statement {
+      effect = "allow"
+      actions = [
+        "execute-api:ManageConnects",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:GetItem",
+        "dynamodb:Scan",
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes"
+      ]
+      resources = [
+        "arn:aws:sqs:${var.aws_region}:${local.account_id}:${var.sqs_queue_name}",
+        "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/${var.websocket_table_name}",
+        "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/${var.vendor_table_name}",
+        # api gateway
+      ]
+    }
+}
+
+resource "aws_iam_policy" "lambda_ws" {
+    name = "${var.app_name}-lambda-ws"
+    policy = data.aws_iam_policy_document.lambda_ws.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ws" {
+    policy_arn = aws_iam_policy.lambda_ws.arn
+    role = aws_iam_role.lambda_main.name   
+}

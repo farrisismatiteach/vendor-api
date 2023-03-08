7fad3938eed264050501509842f8710c26329358
farrisismatiteach <farris.ismati.teach@gmail.com>
2023-03-08 10:47:26 -0500

    fix capitalization

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,7 +2,7 @@
 data "aws_iam_policy_document" "assume_role_policy" {
     statement {
       actions = ["sts:AssumeRole"]
-      effect="allow"
+      effect="Allow"
       principals {
         type = "Service"
         identifiers = ["lambda.amazonaws.com"]
